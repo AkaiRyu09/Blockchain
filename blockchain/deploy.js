cd862c4e0f6071167e53d8f191b9352bc43ab94d
Ömer (Yaldırım) <omer.yaldirim@gmail.com>
2023-06-25 21:39:48 +0300

    Create deploy.js

---
+++ blockchain/deploy.js
@@ -0,0 +1,22 @@
+const assert = require('assert');
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const eF = require('./Build/ElectionFact.json');
+// blocks are created with temp gas
+const provider = new HDWalletProvider(
+	'soda primary wheel try parrot such unfair swarm obvious collect tobacco blouse',
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+	const accounts = await web3.eth.getAccounts();
+
+	console.log('Attemping to deploy from account', accounts[0]);
+
+	const result = await new web3.eth.Contract(JSON.parse(eF.interface))
+		.deploy({ data: '0x' + eF.bytecode })
+		.send({ gas: '3000000', from: accounts[0] });
+
+	console.log('Contract deployed to: ', result.options.address);
+};
+deploy();
