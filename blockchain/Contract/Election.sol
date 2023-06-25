cceca0fa22dcd388f4ba682df0fe9aff4699dd4c
Doğukan Kökce <88978496+dgknkokce@users.noreply.github.com>
2023-06-25 18:47:46 +0000

    struct, modifier and constructor added.

--- blockchain/Contract/Election.sol
+++ blockchain/Contract/Election.sol
@@ -26,3 +26,35 @@ contract ElectionFact {
             return (companyEmail[email].deployedAddress,companyEmail[email].el_n,companyEmail[email].el_d);
     }
 }
+
+contract Election {
+
+    //election_authority's address
+    address election_authority;
+    string election_name;
+    string election_description;
+    bool status;
+
+    //election_authority's address taken when it deploys the contract
+    constructor(address authority , string name, string description) public {
+        election_authority = authority;
+        election_name = name;
+        election_description = description;
+        status = true;
+    }
+
+    //Only election_authority can call this function
+    modifier owner() {
+        require(msg.sender == electionauthority, "Error: Access Denied.");
+        ;
+    }
+    //candidate election_description
+
+    struct Candidate {
+        string candidate_name;
+        string candidate_description;
+        string imgHash;
+        uint8 voteCount;
+        string email;
+    }
+}
